5125b51623517a26286c4d4c7f0881a1107cb48d
Paul Knittel <paulknittel@live.com>
2014-04-01 17:10:09 +1000

    Added Datepicker

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ $(function($){
       return Array.prototype.reduce.call(this, callback, initial);
     }
 
-
+  // Setup location modal
   $('.add-location').click(function(){
     bootbox.dialog({
-      message: "Insert html for location filter here!<input class='location'/>",
-      title: "Add a filter by location.",
+      message: "Insert html for location filter here!<br><input placeholder='Add your city' class='location'/>",
+      title: "Add a location filter.",
       buttons: {
         default: {
           label: "Cancel",
           className: "btn-default"
         },
         success: {
-          label: "Insert Location Filter",
+          label: "Apply Location Filter",
           className: "btn-success",
           callback: function() {
               var txt = $('input.location').val();
@@ -26,25 +26,72 @@ $(function($){
         }
       }
     });
+  });
+
+  // Setup date modal
+
+  $('.add-date').click(function(){
+    bootbox.dialog({
+      message: "Insert jQuery datepicker here.<div class='datepicker1'></div><div class='datepicker2'></div>",
+      title: "Add a date filter.",
+      buttons: {
+        default: {
+          label: "Cancel",
+          className: "btn-default"
+        },
+        success: {
+          label: "Apply Date Filter",
+          className: "btn-success",
+          callback: function() {
+              var txt = datepicker1.val() + " - " + datepicker2.val();
+              $('.input input').before('<span class="tag">'+ txt +'</span>');
+              fitInput();
+          }
+        }
+      }
+    });
+    var datepicker1 = $('.datepicker1').datepicker({
+          inline:true,
+          defaultDate: "+1w",
+          changeMonth: true,
+          onClose: function( selectedDate ) {
+            datepicker2.datepicker( "option", "minDate", selectedDate );
+          }
+      });
+    var datepicker2 = $('.datepicker2').datepicker({
+        inline:true,
+        defaultDate: "+1w",
+        changeMonth: true,
+        onClose: function( selectedDate ) {
+          datepicker1.datepicker( "option", "maxDate", selectedDate );
+        }
+      });
   })
 
 
+  $('.search').click(search)
+  function search() {
+    // Get contents from tags
+    var tagsContent = $('.input span.tag').reduce(function(pV, cV, i, a){
+        return pV + $(cV).html() + " ";
+    }, "")
+
+    var inputContent = $('.input input').val();
+
+    console.log('Search started: ', tagsContent, inputContent);
+  }
 
 
-  // $('.input input').on('focusout',function(){
-  //   var txt= this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g,''); // allowed characters
-  //   if(txt) {
-  //     $(this).before('<span class="tag">'+ txt.toLowerCase() +'</span>');
-  //     fitInput();
-  //   }
-  //   this.value="";
-  // }).on('keyup',function( e ){
-  //   // if: comma,enter (delimit more keyCodes with | pipe)
-  //   if(/(188|13)/.test(e.which)) $(this).focusout();
+  // Search if the user presses enter.
+  $('.input input').on('keyup',function( e ){
+        if(/(13)/.test(e.which)) {
+          $(this).focusout();
+          search();
+        }
 
-  // });
+  });
 
-
+  // Deleting tags/filters
   $('.input').on('click','.tag',function(el){
      bootbox.confirm("Really delete this tag?", function(result){
         if (result) {
