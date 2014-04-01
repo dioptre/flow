124a91dd30126c3e5e1976d1751cf69cba54ceac
Paul Knittel <paulknittel@live.com>
2014-04-01 14:43:46 +1000

    Added modals to add basic location & Bootstrap & Icons

--- script.js
+++ script.js
@@ -1,41 +1,80 @@
 $(function($){
 
+  // Extend jQuery to support reduce function
   $.fn.reduce = function(callback, initial){
       return Array.prototype.reduce.call(this, callback, initial);
     }
 
-  $('.input input').on('focusout',function(){
-    var txt= this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g,''); // allowed characters
-    if(txt) {
-      $(this).before('<span class="tag">'+ txt.toLowerCase() +'</span>');
-      fitInput();
-    }
-    this.value="";
-  }).on('keyup',function( e ){
-    // if: comma,enter (delimit more keyCodes with | pipe)
-    if(/(188|13)/.test(e.which)) $(this).focusout();
 
+  $('.add-location').click(function(){
+    bootbox.dialog({
+      message: "Insert html for location filter here!<input class='location'/>",
+      title: "Add a filter by location.",
+      buttons: {
+        default: {
+          label: "Cancel",
+          className: "btn-default"
+        },
+        success: {
+          label: "Insert Location Filter",
+          className: "btn-success",
+          callback: function() {
+              var txt = $('input.location').val();
+              $('.input input').before('<span class="tag">'+ txt +'</span>');
+              fitInput();
+          }
+        }
+      }
+    });
+  })
+
+
+
+
+  // $('.input input').on('focusout',function(){
+  //   var txt= this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g,''); // allowed characters
+  //   if(txt) {
+  //     $(this).before('<span class="tag">'+ txt.toLowerCase() +'</span>');
+  //     fitInput();
+  //   }
+  //   this.value="";
+  // }).on('keyup',function( e ){
+  //   // if: comma,enter (delimit more keyCodes with | pipe)
+  //   if(/(188|13)/.test(e.which)) $(this).focusout();
+
+  // });
+
+
+  $('.input').on('click','.tag',function(el){
+     bootbox.confirm("Really delete this tag?", function(result){
+        if (result) {
+          $(el.toElement).remove(); // Delete the tag clicked
+          fitInput();
+        }
+     })
   });
 
 
-  $('.input').on('click','.tag',function(){
-     if(confirm("Really delete this tag?")) $(this).remove();
-  });
-
-  fitInput();
+  // This function is used to adjust size of input box depending on tags
   function fitInput() {
+
+    // Select input elemet
     var $input = $('.input input');
 
+    // Get size of parent
     var parentLength = $input.parent().innerWidth();
 
-    var tagsLength = $('.input span.tag').reduce(
-        function(pV, cV, i, a){
+    // Length of all Tags
+    var tagsLength = $('.input span.tag').reduce(function(pV, cV, i, a){
               return pV + $(cV).outerWidth(true);
          }, 0)
 
-     // Set input.length = parent - tags
-     $input.width(parentLength - tagsLength - 32);
+    // Set input.length = parent - tags
+    $input.width(parentLength - tagsLength - 32);
 
-  }
+  };
+
+  // Run on initalize
+  fitInput();
 
 });
