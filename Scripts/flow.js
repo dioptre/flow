7274c6473b242651df2178f7a038064338862ea6
Andrew Grosser <dioptre@gmail.com>
2014-07-15 13:18:30 +1000

    forms almost working

--- Scripts/flow.js
+++ Scripts/flow.js
@@ -1,3 +1,5 @@
+
+
 // Analytics using segment
 window.analytics = window.analytics || [], window.analytics.methods = ["identify", "group", "track", "page", "pageview", "alias", "ready", "on", "once", "off", "trackLink", "trackForm", "trackClick", "trackSubmit"], window.analytics.factory = function (t) { return function () { var a = Array.prototype.slice.call(arguments); return a.unshift(t), window.analytics.push(a), window.analytics } }; for (var i = 0; i < window.analytics.methods.length; i++) { var key = window.analytics.methods[i]; window.analytics[key] = window.analytics.factory(key) } window.analytics.load = function (t) { if (!document.getElementById("analytics-js")) { var a = document.createElement("script"); a.type = "text/javascript", a.id = "analytics-js", a.async = !0, a.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.io/analytics.js/v1/" + t + "/analytics.min.js"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(a, n) } }, window.analytics.SNIPPET_VERSION = "2.0.9";
 
@@ -809,7 +811,7 @@ App.Search = DS.Model.extend({
         return '/share/file/' + this.get('ReferenceID');
     }.property('ReferenceID'),
      "ResourcePreviewPath": function(){
-        return '/share/preview/' + this.get('ReferenceID');
+        return '/share/preview/' + this.get('ReferenceID') + '?width=30&height=30&crop=true';
     }.property('ReferenceID'),
     "Author": DS.attr(''),
     "Updated": DS.attr(''),
@@ -1461,7 +1463,6 @@ App.GraphRoute = Ember.Route.extend({
 
 });
 
-
 App.GraphController = Ember.ObjectController.extend({
     queryParams: ['workflowID'],
     newName: null,
@@ -1782,6 +1783,16 @@ App.GraphController = Ember.ObjectController.extend({
     }
 });
 
+
+App.GraphView = Ember.View.extend({
+    didInsertElement : function(){
+        this._super();
+        Ember.run.scheduleOnce('afterRender', this, function () {
+            window.cleanFunctions(this.$());
+            window.renderFunctions(this.$());
+        });
+    }
+});
 
 function recurseGraphData(id, array, _this, depth, depthMax, nodeMax, store) {
     //AGTODO
@@ -3007,8 +3018,8 @@ App.TinymceEditorComponent = Ember.Component.extend({
         //});
 
         // Setup plugins and toolbar
-        config.plugins = ["locationpicker myfilepicker code link noneditable"];
-        config.toolbar = ["undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent code link | locationpicker | myfilepicker"];
+        config.plugins = ["locationpicker myfilepicker code link noneditable myformeditor"];
+        config.toolbar = ["undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent code link | locationpicker | myfilepicker | myformeditor"];
         config.schema = "html5";
         config.menubar = false;
         config.valid_elements = "*[*]";
