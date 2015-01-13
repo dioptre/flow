5e4f0ac55dc77802bb59c8e13ac79299ecfb6741
Andy <andrew@expedit.com.au>
2015-01-13 04:17:30 +0000

    added required objects for continuation

--- Models/Continuation.cs
+++ Models/Continuation.cs
@@ -8,6 +8,24 @@ namespace EXPEDIT.Flow.Models
     public class Continuation
     {
 
+        [Flags]
+        public enum RelationshipType : uint 
+        {
+            Parent = 0,
+            Peer = 1,
+            Child = 2
+        }
+
         public Guid? OldStepID { get; set; }
+        
+        //Single
+        public Guid? NewCompanyID {get;set;}
+        public Guid? NewContactID {get;set;}
+        public Guid? NewWorkflowID { get; set; }
+
+        //Multiple
+        public int? CompanyLevel { get; set; }
+        public uint? Relationship { get; set; } //*REQUIRED: Parent,Peer,Child as above
+
     }
 }
