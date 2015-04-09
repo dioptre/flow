6730b8225bb733111cce06645b42ec6bcdaacf6a
Andrew Grosser <dioptre@gmail.com>
2015-04-09 00:02:56 -0700

    updated translations

---
+++ Services/ITranslationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using Orchard;
+using System.ServiceModel;
+
+namespace EXPEDIT.Flow.Services
+{
+    [ServiceContract]
+    public interface ITranslationService : IDependency
+    {
+        [OperationContract]
+        bool CreateLocale(EXPEDIT.Flow.ViewModels.LocaleViewModel m);
+        [OperationContract]
+        bool DeleteLocale(EXPEDIT.Flow.ViewModels.LocaleViewModel m);
+        [OperationContract]
+        bool DeleteTranslation(EXPEDIT.Flow.ViewModels.TranslationViewModel m);
+        [OperationContract]
+        bool GetLocale(EXPEDIT.Flow.ViewModels.LocaleViewModel m);
+        [OperationContract]
+        bool GetTranslation(EXPEDIT.Flow.ViewModels.TranslationViewModel m);
+        [OperationContract]
+        bool UpdateLocale(EXPEDIT.Flow.ViewModels.LocaleViewModel m);
+        [OperationContract]
+        bool UpdateTranslation(EXPEDIT.Flow.ViewModels.TranslationViewModel m);
+    }
+}
